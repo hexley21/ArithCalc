ebb49495de481e76fcc490a8699c8df979dfeed5
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-08-08 20:16:30 +0400

    feat(equation-history): add click-listener for sheet-buttons

--- app/src/main/java/com/hxl/arithcalc/presentation/fragment/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/hxl/arithcalc/presentation/fragment/calculator/CalculatorFragment.kt
@@ -5,11 +5,13 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.text.InputType
+import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputConnection
+import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -25,10 +27,22 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlin.math.ceil
 
 @AndroidEntryPoint
-class CalculatorFragment : Fragment() {
+class CalculatorFragment : Fragment(), View.OnClickListener {
     private lateinit var binding: FragmentCalculatorBinding
     private val vm: CalculatorViewModel by viewModels()
+    private val keyValues = SparseArray<String>()
     private val themeDialog = ThemeDialog()
+    private lateinit var ic: InputConnection
+
+    private lateinit var btnPi: Button
+    private lateinit var btnBracketOpen: Button
+    private lateinit var btnBracketClose: Button
+    private lateinit var btnRoot: Button
+    private lateinit var btnSin: Button
+    private lateinit var btnCos: Button
+    private lateinit var btnTan: Button
+    private lateinit var btnCot: Button
+    private lateinit var btnLog: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -40,6 +54,7 @@ class CalculatorFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initKeyboard()
         initBottomSheet()
+        initSheetKeyboard()
         initNavigation()
     }
 
@@ -51,7 +66,7 @@ class CalculatorFragment : Fragment() {
         editText.setTextIsSelectable(true)
         editText.showSoftInputOnFocus = false
 
-        val ic: InputConnection = editText.onCreateInputConnection(EditorInfo())
+        ic = editText.onCreateInputConnection(EditorInfo())
         keyboard.setConnection(ic, binding.resultField, vm.insertEquationHistory)
     }
 
@@ -113,10 +128,46 @@ class CalculatorFragment : Fragment() {
         }
     }
 
+    private fun initSheetKeyboard() {
+        btnPi = binding.btnPi
+        btnBracketOpen = binding.btnBracketOpen
+        btnBracketClose = binding.btnBracketClose
+        btnRoot = binding.btnRoot
+        btnSin = binding.btnSin
+        btnCos = binding.btnCos
+        btnTan = binding.btnTan
+        btnCot = binding.btnCot
+        btnLog = binding.btnLog
+
+        btnPi.setOnClickListener(this)
+        btnBracketOpen.setOnClickListener(this)
+        btnBracketClose.setOnClickListener(this)
+        btnRoot.setOnClickListener(this)
+        btnSin.setOnClickListener(this)
+        btnCos.setOnClickListener(this)
+        btnTan.setOnClickListener(this)
+        btnCot.setOnClickListener(this)
+        btnLog.setOnClickListener(this)
+
+        keyValues.put(btnPi.id, getString(R.string.btn_pi))
+        keyValues.put(btnBracketOpen.id, getString(R.string.btn_bracket_open))
+        keyValues.put(btnBracketClose.id, getString(R.string.btn_bracket_close))
+        keyValues.put(btnRoot.id, getString(R.string.btn_root) + "(")
+        keyValues.put(btnSin.id, getString(R.string.btn_sin) + "(")
+        keyValues.put(btnCos.id, getString(R.string.btn_cos) + "(")
+        keyValues.put(btnTan.id, getString(R.string.btn_tan) + "(")
+        keyValues.put(btnCot.id, getString(R.string.btn_cot) + "(")
+        keyValues.put(btnLog.id, getString(R.string.btn_log) + "(")
+    }
+
     override fun onStop() {
         super.onStop()
         if (themeDialog.isStateSaved || themeDialog.isAdded || themeDialog.isVisible) {
             themeDialog.dismissAllowingStateLoss()
         }
+    }
+
+    override fun onClick(v: View) {
+        ic.commitText(keyValues[v.id], 1)
     }
 }
